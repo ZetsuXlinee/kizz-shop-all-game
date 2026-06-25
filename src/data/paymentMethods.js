ed7b09ced333d9556bb4d970645f4809e1228cc8
ZetsuXlinee <dzikozik@gmail.com>
2026-06-26 02:45:44 +0700

    Add files via upload

---
+++ src/data/paymentMethods.js
@@ -0,0 +1,30 @@
+export const paymentMethods = [
+  { 
+    id: 'qris', 
+    name: 'QRIS', 
+    image: '/payment/qris.jpg', 
+    fee: '0%',
+    status: 'Otomatis'
+  },
+  { 
+    id: 'dana', 
+    name: 'DANA', 
+    image: '/payment/dana.jpg', 
+    fee: 'Rp 500',
+    status: 'Otomatis'
+  },
+  { 
+    id: 'gopay', 
+    name: 'GoPay', 
+    image: '/payment/gopay.jpg', 
+    fee: '2%',
+    status: 'Otomatis'
+  },
+  { 
+    id: 'ovo', 
+    name: 'OVO', 
+    image: '/payment/ovo.jpg', 
+    fee: 'Rp 1.000',
+    status: 'Otomatis'
+  }
+];
